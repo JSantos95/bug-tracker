8a076f13256d8259ebd9a6a81c678872c380fc94
Jose <santosjosel1995@gmail.com>
2021-10-03 15:44:30 -0400

    feat(): UI for adding a new user

--- src/components/createUser.js
+++ src/components/createUser.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const CreateUser = () => {
+    const [username, setUsername] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const user = {
+            username,
+        }
+        console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then((res) => console.log(res.data));
+    }
+
     return (
-        <div>Create User</div>
+        <div>
+            <h2>Create A New User</h2>
+            <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="username">Username: </label>
+                    <input type="text" value={username} name="username" 
+                        className="form-control" placeholder="Enter Username"
+                        onChange={e => setUsername(e.target.value)} 
+                    />
+                </div>
+
+                <input className="btn btn-primary" type="submit"/>
+            </form>
+        </div>
     )
 }
 
