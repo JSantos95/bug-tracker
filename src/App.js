94363e004630ed5909b3f296cc74de02399a2c33
Jose <santosjosel1995@gmail.com>
2021-10-05 16:30:48 -0400

    update(): renamed Edit to View

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from './components/navbar';
 import BugList from './components/bugList';
-import EditBug from './components/editBug';
+import ViewBug from './components/viewBug';
 import CreateBug from './components/createBug';
 import CreateUser from './components/createUser';
 
@@ -15,7 +15,7 @@ const App = () => {
         <NavBar />
         <br/>
         <Route path="/" exact component={BugList}/>
-        <Route path="/edit/:id" component={EditBug}/>
+        <Route path="/view/:id" component={ViewBug}/> 
         <Route path="/create" component={CreateBug}/>
         <Route path="/user" component={CreateUser}/>
       </Router>
