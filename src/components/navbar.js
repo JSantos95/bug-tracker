a40782738c0186ad86428c141b4f15feb74327d2
Jose <santosjosel1995@gmail.com>
2021-11-05 10:41:48 -0500

    update(): Home will be the bug list if signed in

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link to="/" className="navbar-brand">Bug Tracker</Link>
+                <Link to={isToken ? "/bug" : "/" } className="navbar-brand">Bug Tracker</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
